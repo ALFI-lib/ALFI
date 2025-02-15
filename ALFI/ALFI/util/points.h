206f0ad953223932f312bed281628d8cdc4af6e4
Petr Sabanov <pasabanov@murena.io>
2025-02-15 12:42:41 +0300

    Added type specification to functions in `alfi::points`

--- ALFI/ALFI/util/points.h
+++ ALFI/ALFI/util/points.h
@@ -16,7 +16,7 @@ namespace alfi::points {
 	template <typename Number = DefaultNumber, template <typename> class Container = DefaultContainer>
 	Container<Number> lin_mapped(const Container<Number>& points, Number a, Number b, Number c, Number d) {
 		auto mapped_points = points;
-		lin_map(mapped_points, a, b, c, d);
+		lin_map<Number,Container>(mapped_points, a, b, c, d);
 		return mapped_points;
 	}
 
@@ -29,7 +29,7 @@ namespace alfi::points {
 			std::fill(points.begin(), points.end(), (a + b) / 2);
 			return;
 		}
-		lin_map(points, points.front(), points.back(), a, b);
+		lin_map<Number,Container>(points, points.front(), points.back(), a, b);
 		points.front() = a;
 		points.back() = b;
 	}
@@ -37,7 +37,7 @@ namespace alfi::points {
 	template <typename Number = DefaultNumber, template <typename> class Container = DefaultContainer>
 	Container<Number> stretched(const Container<Number>& points, Number a, Number b) {
 		auto stretched_points = points;
-		stretch(stretched_points, a, b);
+		stretch<Number,Container>(stretched_points, a, b);
 		return stretched_points;
 	}
 }
